7e03b0cb7fa658c0989df55c199f3adc4b7c5e69
giulio-zhou <giuliozhou8@gmail.com>
2014-11-08 12:19:32 -0800

    Fixed broken url

--- django_test/settings.py
+++ django_test/settings.py
@@ -129,6 +129,10 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 	'page',
+	'resources',
+	'home',
+	'house_info',
+	'rush',
 )
 
 # A sample logging configuration. The only tangible logging
