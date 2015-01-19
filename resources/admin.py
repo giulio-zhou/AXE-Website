8207cd41e8cf74670e5f503e475c021df5add072
Giulio Zhou <giuliozhou@Giulios-MacBook-Pro.local>
2015-01-19 13:42:31 -0800

    Enabled exam approval from admin interface

--- resources/admin.py
+++ resources/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from resources.models import Exam
 
-admin.site.register(Exam)
+def approve_exam(modeladmin, request, queryset):
+    queryset.update(approved=True)
+approve_exam.short_description = "Mark following exams as approved"
+
+class ExamAdmin(admin.ModelAdmin):
+    list_display = ['instructor', 'key', 'term', 'subject', 'course', \
+                    'year', 'exam', 'approved']
+    ordering = ['course']
+    actions = [approve_exam]
+
+admin.site.register(Exam, ExamAdmin)
--- resources/models.py
+++ resources/models.py
@@ -3,14 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Exam(models.Model):
-	instructor = models.CharField(max_length=100)
-	key = models.BooleanField(default=False)
-	term = models.CharField(max_length=50)
-	subject = models.CharField(max_length=50)
-	course = models.CharField(max_length=50)
-	year = models.IntegerField()
-	pdf_path = models.CharField(max_length=100)
-	exam = models.CharField(max_length=50)
-	def __unicode__(self):
-		return self.pdf_path
+    instructor = models.CharField(max_length=100)
+    key = models.BooleanField(default=False)
+    term = models.CharField(max_length=50)
+    subject = models.CharField(max_length=50)
+    course = models.CharField(max_length=50)
+    year = models.IntegerField()
+    pdf_path = models.CharField(max_length=100)
+    exam = models.CharField(max_length=50)
+    approved = models.BooleanField(default=False)
+    def __unicode__(self):
+        return self.pdf_path
 
